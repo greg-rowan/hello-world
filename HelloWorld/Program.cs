1ce44a1b8581c58fafd9021bf10507e0c634947c
greg-rowan <greg.rowan@macys.com>
2015-12-22 23:04:36 -0500

    updated string

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World. Press any key to continue....");
+            Console.WriteLine("Hello World. Press any key to continue.....");
             Console.ReadKey();
         }
     }
