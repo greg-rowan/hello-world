beb5be69d113311c928be23eb5377ce537a82038
greg-rowan <greg.rowan@macys.com>
2015-12-22 22:33:47 -0500

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World. Press any key to continue....");
+            Console.ReadKey();
+        }
+    }
+}
